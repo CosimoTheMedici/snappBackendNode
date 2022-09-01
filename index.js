11680e1d273ce559617bc709d92d0ed813418c66
cosimo_de_medici <cosmasthuku4@gmail.com>
2022-09-01 09:43:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = 5050;
 
 app.use(bodyParser.json());
 app.use(cors())
